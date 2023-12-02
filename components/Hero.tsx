b483fe74289c1463d40afdccd8f75aa4e37236d5
robinhood6996 <robinypb@yahoo.com>
2023-12-02 13:35:24 +0600

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+export default function Hero({}: Props) {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">Hero</h1>
+      </div>
+    </div>
+  );
+}
